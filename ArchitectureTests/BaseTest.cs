9924494af933ac672406c51bd161b5a10e43e1a0
Adam Nilsson <nooxie94@gmail.com>
2026-04-17 10:17:15 +0200

    Initialize project structure with API, Application, Domain, and Infrastructure layers. Add architecture tests and build configurations. Include Docker support and basic project settings.

---
+++ ArchitectureTests/BaseTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace ArchitectureTests;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(Domain.IMarker).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(Application.IMarker).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(Infrastructure.IMarker).Assembly;
+    protected static readonly Assembly PresentationAssembly = typeof(Api.IMarker).Assembly;
+}
